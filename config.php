14a3252e831ab55b8c8251a9b62584edb8c9918f
worthy-elevator <emil_96@inbox.ru>
2018-04-30 01:06:51 +0300

    ночной дебаг

--- config.php
+++ config.php
@@ -1,9 +1,14 @@
 <?
-$db_host='localhost:3306'; 
+/*$db_host='localhost:3306'; 
 $db_name='u0499678_site'; 
 $db_user='u0499_patrikRoot'; 
 $db_pass='magicPASSWORD_f1';
+*/
 
+$db_host='localhost'; 
+$db_name='site'; 
+$db_user='root'; 
+$db_pass='';
  
 
 
