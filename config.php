16566c8c251ccaab22e7af5a725493dbee1245ed
worthy-elevator <emil_96@inbox.ru>
2018-04-30 01:17:52 +0300

    тупые тупняки

--- config.php
+++ config.php
@@ -1,17 +1,12 @@
 <?
-/*$db_host='localhost:3306'; 
+$db_host='localhost:3306'; 
 $db_name='u0499678_site'; 
 $db_user='u0499_patrikRoot'; 
 $db_pass='magicPASSWORD_f1';
-*/
 
-$db_host='localhost'; 
-$db_name='site'; 
-$db_user='root'; 
-$db_pass='';
  
 
-/*
+
 function setupDBConnect() {
 	
 	$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
@@ -21,5 +16,5 @@ function setupDBConnect() {
 	}
 	return $mysqli;
 }
-*/
+
 ?>
