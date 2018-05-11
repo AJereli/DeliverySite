64b73553ead480dfe560f000acbd7edda80d00c9
worthy-elevator <emil_96@inbox.ru>
2018-05-11 17:05:38 +0300

    хэдрр

--- config.php
+++ config.php
@@ -1,7 +1,11 @@
 <?
-$db_host='localhost:3306'; 
+/*$db_host='localhost:3306'; 
 $db_name='u0504700_site'; 
 $db_user='u0504700_root'; 
-$db_pass='magicPASSWORD_f1';
+$db_pass='magicPASSWORD_f1';*/
+$db_host='localhost'; 
+$db_name='site'; 
+$db_user='root'; 
+$db_pass='';
 
 ?>
