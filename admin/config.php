0bf7f8d7046e3c79fd9902ee20f78eda28921fa9
AJereli <drefaz@gmail.com>
2018-05-12 14:16:13 +0300

    sort

--- admin/config.php
+++ admin/config.php
@@ -84,6 +84,9 @@ function printSideMenu(){
                     <li>
                         <a href="type.php"><i class="fa fa-desktop "></i>Добавить тип</a>
                     </li>
+					  <li>
+                        <a href="settings.php"><i class="fa fa-desktop "></i>Настройки</a>
+                    </li>
                     <li>
                         <a href=""><i class="fa fa-edit "></i>Страница в разработке</a>
                     </li>
